1f39aaa0a03e47ddfc4185365296c774365efc56
masinc <masinc000@gmail.com>
2022-11-21 01:45:22 +0900

    first commit

---
+++ aivoice-cli-fs/Program.fs
@@ -0,0 +1,154 @@
+﻿open System.Collections.Generic
+open System.Text.Encodings.Web
+open System.Text.Json
+open System.Text.Unicode
+open AI.Talk.Editor.Api
+open CommandLine
+open CommandLine.Text
+
+[<RequireQualifiedAccess>]
+type OutputFormat =
+    | text = 0
+    | json = 1
+
+[<Verb("list-char")>]
+type CliListChar =
+    { [<Option('o', "output", Default = OutputFormat.text)>]
+      output: OutputFormat }
+    member this.run(tts: TtsControl) =
+        let names = tts.VoiceNames
+
+        match this.output with
+        | OutputFormat.text ->
+            names
+            |> Array.iter (fun name -> printfn $"{name}")
+        | OutputFormat.json ->
+            let opt = JsonSerializerOptions()
+            opt.Encoder <- JavaScriptEncoder.Create UnicodeRanges.All
+            opt.WriteIndented <- true
+            let s = JsonSerializer.Serialize(names, opt)
+            printfn $"{s}"
+        | _ -> failwith "unreachable"
+
+        0
+
+[<Verb("list-preset")>]
+type CliListPreset =
+    { [<Option('o', "output", Default = OutputFormat.text)>]
+      output: OutputFormat }
+    member this.run(tts: TtsControl) =
+        let names = tts.VoicePresetNames
+
+        match this.output with
+        | OutputFormat.text ->
+            names
+            |> Array.iter (fun name -> printfn $"{name}")
+        | OutputFormat.json ->
+            let opt = JsonSerializerOptions()
+            opt.Encoder <- JavaScriptEncoder.Create UnicodeRanges.All
+            opt.WriteIndented <- true
+            let s = JsonSerializer.Serialize(names, opt)
+            printfn $"{s}"
+        | _ -> failwith "unreachable"
+
+        0
+
+[<Verb("info")>]
+type CliInfo =
+    { [<Option('v', "verbose", Default = false)>]
+      verbose: bool }
+    member this.run(tts: TtsControl) =
+        let j =
+            Schema.MasterControl.from_json tts.MasterControl
+
+        printfn $"{j.to_json ()}"
+        0
+
+[<Verb("play")>]
+type CliPlay() =
+    [<Option('n', "name")>]
+    member val name: string = null with get, set
+
+    [<Option('t', "text", Group = "input")>]
+    member val text: string = null with get, set
+
+    [<Option('f', "file", Group = "input")>]
+    member val file: string = null with get, set
+
+    member this.run(tts: TtsControl) =
+        let text =
+            if this.text <> null then
+                this.text
+            else
+                System.IO.File.ReadAllText this.file
+
+        if this.name <> null then
+            tts.CurrentVoicePresetName <- this.name
+
+        tts.TextEditMode <- TextEditMode.Text
+        tts.Text <- text
+        tts.Play()
+        0
+
+[<Verb("save")>]
+type CliSave() =
+    [<Option('n', "name")>]
+    member val name: string = null with get, set
+
+    [<Option('t', "text", Group = "input")>]
+    member val text: string = null with get, set
+
+    [<Option('f', "file", Group = "input")>]
+    member val file: string = null with get, set
+
+    [<Option('o', "output", Required = true)>]
+    member val output: string = null with get, set
+    
+    member this.run(tts: TtsControl) =
+        let text =
+            if this.text <> null then
+                this.text
+            else
+                System.IO.File.ReadAllText this.file
+        if this.name <> null then
+            tts.CurrentVoicePresetName <- this.name
+        
+        tts.TextEditMode <- TextEditMode.Text
+        tts.Text <- text
+        
+        tts.SaveAudioToFile(this.output)
+        0
+
+let init_tts (tts: TtsControl) =
+    let hostname =
+        tts.GetAvailableHostNames()[0]
+
+    tts.Initialize hostname
+
+    if tts.Status = HostStatus.NotRunning then
+        tts.StartHost()
+
+[<EntryPoint>]
+let main args =
+    let tts = TtsControl()
+
+    try
+        init_tts tts
+        tts.Connect()
+
+        let result =
+            Parser.Default.ParseArguments<CliListChar, CliListPreset, CliInfo, CliPlay, CliSave> args
+
+        match result with
+        | :? CommandLine.Parsed<obj> as command ->
+            match command.Value with
+            | :? CliListChar as opts -> opts.run tts
+            | :? CliListPreset as opts -> opts.run tts
+            | :? CliInfo as opts -> opts.run tts
+            | :? CliPlay as opts -> opts.run tts
+            | :? CliSave as opts -> opts.run tts
+            | _ -> failwith "unreachable"
+        | :? CommandLine.NotParsed<obj> -> 1
+        | _ -> failwith "unreachable"
+    finally
+        tts.Disconnect()
---
+++ aivoice-cli-fs/Schema.fs
@@ -0,0 +1,60 @@
+﻿module Schema
+
+open System.Text.Encodings.Web
+open System.Text.Json
+open System.Text.Unicode
+
+let DEFAULT_VOLUME = 1.0
+let DEFAULT_PITCH = 1.0
+let DEFAULT_SPEED = 1.0
+let DEFAULT_PITCH_RANGE = 1.0
+let DEFAULT_MIDDLE_PAUSE = 150.0
+let DEFAULT_LONG_PAUSE = 370.0
+let DEFAULT_SENTENCE_PAUSE = 800.0
+
+type MasterControl() =
+    member val Volume: double = DEFAULT_VOLUME with get, set
+    member val Pitch: double = DEFAULT_PITCH with get, set
+    member val Speed: double = DEFAULT_SPEED with get, set
+    member val PitchRange: double = DEFAULT_PITCH_RANGE with get, set
+    member val MiddlePause: double = DEFAULT_MIDDLE_PAUSE with get, set
+    member val LongPause: double = DEFAULT_LONG_PAUSE with get, set
+    member val SentencePause: double = DEFAULT_SENTENCE_PAUSE with get, set
+
+    member this.to_json() : string =
+        let opt = JsonSerializerOptions()
+        opt.Encoder <- JavaScriptEncoder.Create UnicodeRanges.All
+        opt.WriteIndented <- true
+        JsonSerializer.Serialize(this, opt)
+
+    static member from_json(json: string) : MasterControl = JsonSerializer.Deserialize json
+
+
+type Info() =
+    member val name: string = null with get, set
+    member val volume: double = DEFAULT_VOLUME with get, set
+    member val pitch: double = DEFAULT_PITCH with get, set
+    member val speed: double = DEFAULT_SPEED with get, set
+    member val pitch_range: double = DEFAULT_PITCH_RANGE with get, set
+    member val middle_pause: double = DEFAULT_MIDDLE_PAUSE with get, set
+    member val long_pause: double = DEFAULT_LONG_PAUSE with get, set
+    member val sentence_range: double = DEFAULT_SENTENCE_PAUSE with get, set
+
+    member this.to_json() : string =
+        let opt = JsonSerializerOptions()
+        opt.Encoder <- JavaScriptEncoder.Create UnicodeRanges.All
+        opt.WriteIndented <- true
+        JsonSerializer.Serialize(this, opt)
+
+    member this.to_master_control() : MasterControl =
+        MasterControl(
+            Volume = this.volume,
+            Pitch = this.pitch,
+            Speed = this.speed,
+            PitchRange = this.pitch_range,
+            MiddlePause = this.middle_pause,
+            LongPause = this.long_pause,
+            SentencePause = this.sentence_range
+        )
+
+    static member from_json(json: string) : Info = JsonSerializer.Deserialize json
